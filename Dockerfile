fae00124c167e0cfa484b406548f46a409aff037
markgllin <marlin2@cisco.com>
2021-01-22 15:29:32 -0700

    working tensorboard visualization + use zlib dataset

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,6 @@ ENV PYTHONPATH=/home/asm2vec
 RUN apt-get update && \
     apt install python3.8 python3-pip libgraphviz-dev -y
 RUN pip3 install pygraphviz --install-option="--include-path=/usr/include/graphviz" --install-option="--library-path=/usr/lib/x86_64-linux-gnu/graphviz" 
-RUN pip3 install llvmlite networkx numpy sklearn seaborn matplotlib
+RUN pip3 install llvmlite networkx numpy sklearn seaborn matplotlib tensorflow
+
+EXPOSE 6006
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from llvmir.function_cfg import FunctionCFG
 from asm2vec.asm import Function
 from asm2vec.model import Asm2Vec, Asm2VecMemento
 import llvmlite.binding as llvm
+import os 
 import logging
 
 from sklearn.manifold import TSNE
@@ -11,13 +12,18 @@ import seaborn as sns
 import pprint
 import pdb
 
+import numpy as np
+import tensorflow as tf
+
+from tensorboard.plugins import projector
+
 pp = pprint.PrettyPrinter(indent=4)
 # import pdb
 # pdb.set_trace()
 
 logging.basicConfig(level=logging.DEBUG)
 
-f = open("llvm_binary_example/busybox-O0.bc", "rb")
+f = open("llvm2vec_dataset/zlib/arm/zlib-O3/libz/adler32.o.bc", "rb")
 data=f.read()
 f.close()
 
@@ -33,7 +39,7 @@ for raw_func in moduleref.functions:
 
 # train model 
 print("Training model...")
-model = Asm2Vec(d=3)
+model = Asm2Vec(d=200)
 train_repo = model.make_function_repo(functions)
 model.train(train_repo)
 
@@ -64,24 +70,53 @@ for function in functions:
 # exit()
 
 
-print("Generating TSNE plot...")
-tsne = TSNE(n_components=3, random_state=0)
-function_vectors_2d = tsne.fit_transform(function_vectors)
-print(function_vectors_2d)
-# pdb.set_trace()
-df_subset = {}
-df_subset['tsne-2d-one'] = function_vectors_2d[:,0]
-df_subset['tsne-2d-two'] = function_vectors_2d[:,1]
-print(df_subset)
-plt.figure(figsize=(16,10))
-sns.scatterplot(
-    x="tsne-2d-one", y="tsne-2d-two",
-    # hue="y",
-    palette=sns.color_palette("hls", 10),
-    data=df_subset,
-    legend="full",
-    alpha=0.3
-)
-plt.savefig('foo2.png')
+max_size = len(function_vectors)
+print("Number of functions "+ str(max_size))
+func2vec = np.zeros((max_size, function_vectors[0].size))
+print(function_vectors)
+print(func2vec)
+
+if not os.path.exists('projections'):
+  os.makedirs('projections')
+
+with open("projections/metadata.tsv", 'w+') as file_metadata:
+  for i, word in enumerate(function_vectors[:max_size]):
+    func2vec[i] = function_vectors[i]
+    file_metadata.write(str(i) + '\n')
+
+tf.compat.v1.disable_eager_execution()
+sess=tf.compat.v1.InteractiveSession()
+with tf.device("/cpu:0"):
+  embedding = tf.Variable(func2vec, trainable=False, name='embedding')
+
+sess.run(tf.compat.v1.global_variables_initializer())
+saver = tf.compat.v1.train.Saver()
+writer = tf.compat.v1.summary.FileWriter('projections', sess.graph)
+config = projector.ProjectorConfig()
+embed = config.embeddings.add()
+embed.tensor_name = 'embedding'
+embed.metadata_path = 'metadata.tsv'
+projector.visualize_embeddings(writer, config)
+saver.save(sess, 'projections/model.ckpt', global_step=max_size)
+
+# print("Generating TSNE plot...")
+# tsne = TSNE(n_components=3, random_state=0)
+# function_vectors_2d = tsne.fit_transform(function_vectors)
+# print(function_vectors_2d)
+
+# df_subset = {}
+# df_subset['tsne-2d-one'] = function_vectors_2d[:,0]
+# df_subset['tsne-2d-two'] = function_vectors_2d[:,1]
+# print(df_subset)
+# plt.figure(figsize=(16,10))
+# sns.scatterplot(
+#     x="tsne-2d-one", y="tsne-2d-two",
+#     # hue="y",
+#     palette=sns.color_palette("hls", 10),
+#     data=df_subset,
+#     legend="full",
+#     alpha=0.3
+# )
+# plt.savefig('zlib-arm-retdec-03.png')
 
 
